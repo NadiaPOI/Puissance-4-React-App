fa71d217528e011c8b97473a97021329046f4bc7
NadiaPoi <nadia.poi.fou@gmail.com>
2019-07-08 21:42:40 +0200

    update eslint.rc

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-
-function Home() {  
-    return (
-        <div>
-            <h1>Puissance 4</h1>
-            <button id="start" onClick={() => console.log('clicked')}>Start a Game</button>
-        </div>
-    )
+function Home() {
+  return (
+    <div>
+      <h1>Puissance 4</h1>
+      <button type="button" id="start" onClick={() => console.log('clicked')}>
+        Start a Game
+      </button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
