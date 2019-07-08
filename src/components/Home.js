21ed6e56e38ea551b2bcbd6626414e174c1e69da
NadiaPoi <nadia.poi.fou@gmail.com>
2019-07-08 15:58:44 +0200

    add lint travis and Home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Home() {  
+    return (
+        <div>
+            <h1>Puissance 4</h1>
+            <button id="start" onClick={() => console.log('clicked')}>Start a Game</button>
+        </div>
+    )
+}
+
+export default Home
