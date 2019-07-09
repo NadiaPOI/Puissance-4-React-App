e93278d9d2965bc303cefaae8635edd7d40252f8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 15:20:10 +0000

    Deploy NadiaPOI/Puissance-4-React-App to github.com/NadiaPOI/Puissance-4-React-App.git:gh-pages

---
+++ precache-manifest.158b78f824176fa6cf96e64a545c9424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dfdcde0a92311ac5010ecad94d8dbf9",
+    "url": "/NadiaPOI/Puissance4/index.html"
+  },
+  {
+    "revision": "f0126a8ba6d7e539dc25",
+    "url": "/NadiaPOI/Puissance4/static/css/main.7ef2cb30.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/NadiaPOI/Puissance4/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "f0126a8ba6d7e539dc25",
+    "url": "/NadiaPOI/Puissance4/static/js/main.acb04947.chunk.js"
+  },
+  {
+    "revision": "5a6e8c15419c72e31978",
+    "url": "/NadiaPOI/Puissance4/static/js/runtime~main.bae9a5ae.js"
+  }
+]);
