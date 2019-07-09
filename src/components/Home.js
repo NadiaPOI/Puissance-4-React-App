13ac9bf7de881fa866e3005ddc04796b16395199
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 13:00:06 +0000

    Deploy NadiaPOI/Puissance-4-React-App to github.com/NadiaPOI/Puissance-4-React-App.git:gh-pages

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Puissance 4</h1>
+      <button
+        type='button'
+        id='start'
+        onClick={() => console.log('is clicked')}
+      >
+        Start a Game
+      </button>
+    </div>
+  );
+}
+
+export default Home;
