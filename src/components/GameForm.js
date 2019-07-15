15744eeb1ad99777650ee75432f36e5efd60d261
NadiaPoi <nadia.poi.fou@gmail.com>
2019-07-15 08:27:37 +0200

    update GameForm.js

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function GameForm() {
+  const [initialValueRows, setValueRows] = useState(0);
+  const [initialValueColumns, setValueColumns] = useState(0);
+
+  function generateGameboard(lengthRow, lengthColumn) {
+    let gameboard = new Array(lengthRow)
+      .fill(null)
+      .map(() => new Array(lengthColumn).fill(''));
+    return gameboard;
+  }
+
+  function handleChangeRows() {
+    setValueRows(initialValueRows + 1);
+  }
+
+  function handleChangeColumns() {
+    setValueColumns(initialValueColumns + 1);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let gameboard = generateGameboard(initialValueRows, initialValueColumns);
+    console.log(gameboard);
+    return gameboard;
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor='row'>Please choose the number of rows :</label>
       <input
         type='number'
@@ -12,6 +37,8 @@ function GameForm() {
         min='4'
         max='50'
         required
+        value={initialValueRows}
+        onChange={handleChangeRows}
       />
       <label htmlFor='col'>Please choose the number of columns :</label>
       <input
@@ -22,6 +49,8 @@ function GameForm() {
         min='4'
         max='50'
         required
+        value={initialValueColumns}
+        onChange={handleChangeColumns}
       />
       <input type='submit' value='Generate gameboard' />
     </form>
