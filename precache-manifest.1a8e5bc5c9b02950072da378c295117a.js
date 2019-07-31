e10c89ce61bcec107cf603a9fe194b2857a0f15e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 08:11:26 +0000

    Deploy NadiaPOI/Puissance-4-React-App to github.com/NadiaPOI/Puissance-4-React-App.git:gh-pages

---
+++ precache-manifest.1a8e5bc5c9b02950072da378c295117a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aee3fbd8af81e9d4a42ef8c5151c6ac",
+    "url": "/Puissance-4-React-App/index.html"
+  },
+  {
+    "revision": "4d7c49614ffed5f35f21",
+    "url": "/Puissance-4-React-App/static/css/main.65f61345.chunk.css"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/Puissance-4-React-App/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "4d7c49614ffed5f35f21",
+    "url": "/Puissance-4-React-App/static/js/main.abf7de95.chunk.js"
+  },
+  {
+    "revision": "4209314eca0dec12876e",
+    "url": "/Puissance-4-React-App/static/js/runtime~main.bd3c9225.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Puissance-4-React-App/precache-manifest.8b43e3a6adf6597f926e11c1d7ac2081.js"
+  "/Puissance-4-React-App/precache-manifest.1a8e5bc5c9b02950072da378c295117a.js"
 );
 
 self.addEventListener('message', (event) => {
