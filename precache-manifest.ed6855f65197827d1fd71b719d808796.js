6ecca81dd27ffa59e1c35dd9d69b1e3277e390df
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-27 12:14:20 +0000

    Deploy NadiaPOI/Puissance-4-React-App to github.com/NadiaPOI/Puissance-4-React-App.git:gh-pages

--- precache-manifest.ed6855f65197827d1fd71b719d808796.js
+++ precache-manifest.ed6855f65197827d1fd71b719d808796.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18c4258ed5614b202ae9493a99f1eac6",
+    "revision": "09daf50b17cb542443242317eea8d7f0",
     "url": "/Puissance-4-React-App/index.html"
   },
   {
-    "revision": "1d1c9268f1c0fd4ead21",
-    "url": "/Puissance-4-React-App/static/css/main.65f61345.chunk.css"
+    "revision": "f581ac999529803b8764",
+    "url": "/Puissance-4-React-App/static/css/main.2e40fb57.chunk.css"
   },
   {
     "revision": "a2722e2b09ce4a7f3df5",
     "url": "/Puissance-4-React-App/static/js/2.935a9550.chunk.js"
   },
   {
-    "revision": "1d1c9268f1c0fd4ead21",
-    "url": "/Puissance-4-React-App/static/js/main.b28beb0e.chunk.js"
+    "revision": "f581ac999529803b8764",
+    "url": "/Puissance-4-React-App/static/js/main.365fcb41.chunk.js"
   },
   {
     "revision": "4209314eca0dec12876e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Puissance-4-React-App/precache-manifest.b4b44684dde73c2ad25d7ba99a69dbb6.js"
+  "/Puissance-4-React-App/precache-manifest.ed6855f65197827d1fd71b719d808796.js"
 );
 
 self.addEventListener('message', (event) => {
