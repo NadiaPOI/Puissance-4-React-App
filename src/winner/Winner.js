73ab021c80ce00db6b1e518b6d19c0e53cef250f
NadiaPoi <nadia.poi.fou@gmail.com>
2020-01-21 11:47:59 +0100

    update travis.yml and deploy heroku

--- src/winner/Winner.js
+++ src/winner/Winner.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function Winner({player, onClick}) {
+export default function Winner({ player, onClick }) {
   return (
-    <div className='displayWinner'>
-      <p>Great !! {player === 'Y' ? 'Yellow' : 'Red'} player win !</p>
-      <Link className='restart' to='/' onClick={onClick}>
+    <div className="displayWinner">
+      <p>Great !! {player === "Y" ? "Yellow" : "Red"} player win !</p>
+      <Link className="restart" to="/" onClick={onClick}>
         Start again
       </Link>
     </div>
