5648dc1fd964fb21a26a2dc5a4c93e193eaed7ad
unknown <nadia.poi.fou@gmail.com>
2020-02-28 15:26:28 +0100

    return to old state without backend and update css

--- src/game/Home.js
+++ src/game/Home.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 function Home() {
   return (
-    <Link to='/form' className='start'>
+    <Link to='/form' className='button'>
       Start a Game
     </Link>
   );
